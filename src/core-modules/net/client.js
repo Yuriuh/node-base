dd19ae10729ef8356857a655377c0b130f9fefaf
Yuriuh <642192014@qq.com>
2021-04-13 23:54:24 +0800

    完成 parseResponse 组件

--- src/core-modules/net/client.js
+++ src/core-modules/net/client.js
@@ -11,22 +11,34 @@ const socket = net.createConnection(
   () => {
     console.log('Connected successfully.')
   }
-)``
+)
 
 let isFirst = true
+let receive = null
 
 const parseResponse = response => {
   const index = response.indexOf('\r\n\r\n')
   const head = response.slice(0, index)
-  const body = response.slice(index + 1)
+  const body = response.slice(index + 2)
   const headParts = head.split('\r\n')
-  console.log('head', head)
-  console.log('headParts', headParts)
-  console.log('body', body)
+  const headerArray = headParts.slice(1).map(str => {
+    return str.split(':').map(s => s.trim())
+  })
+  const header = headerArray.reduce((a, b) => {
+    a[b[0]] = b[1]
+    return a
+  }, {})
+  return {
+    header,
+    body: body.trimStart(),
+  }
 }
 
 socket.on('data', chunk => {
   const response = chunk.toString('utf-8')
+  // if (!receive) {
+
+  // }
   if (isFirst) {
     parseResponse(response)
     isFirst = false
